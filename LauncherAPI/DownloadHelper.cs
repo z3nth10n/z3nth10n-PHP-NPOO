eeedd02341aaffa9fb30ec853a1d1d5ce2dafd12
Ikillnukes <ikillnukes1@gmail.com>
2018-05-07 02:49:07 +0200

    Private key removed

---
+++ LauncherAPI/DownloadHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace LauncherAPI
+{
+    public static class DownloadHelper
+    {
+        public readonly static FileDownloader downloader = new FileDownloader();
+
+        public static void DownloadFile(string path, string url, bool overwrite = false)
+        {
+            if (!File.Exists(path) || overwrite)
+                downloader.Files.Add(new FileDownloader.FileInfo(path, url));
+            else
+                Console.WriteLine("File '{0}' already exists! Skipping... (Path: {1})", Path.GetFileName(path), path);
+        }
+
+        public static string DownloadSyncFile(string path, string url, bool overwrite = false)
+        {
+            if (ApiBasics.PreviousChk(path) || overwrite)
+            {
+                Console.WriteLine("Downloading '{0}' from '{1}', please wait...", Path.GetFileName(path), url.CleverSubstring());
+                try
+                {
+                    using (WebClient wc = new WebClient())
+                        wc.DownloadFile(new Uri(url), path);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("There was a problem downloading {0}...", url);
+                    Console.WriteLine(ex);
+                }
+            }
+            else
+                Console.WriteLine("File '{0}' already exists! Skipping... (Path: {1})", Path.GetFileName(path), path);
+
+            return path;
+        }
+    }
+}
---
+++ LauncherAPI/ThreadHelper.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Forms;
+
+namespace LauncherAPI
+{
+    public static class ThreadHelper
+    {
+#if MONO
+        private delegate void SetTextCallback<T>(Form f, T ctrl, string property, object obj) where T : Control;
+#else
+        private delegate void SetTextCallback<T>(Form f, in T ctrl, string property, object obj) where T : Control;
+#endif
+
+        /// <summary>
+        /// Set text property of various controls
+        /// </summary>
+        /// <param name="form">The calling form</param>
+        /// <param name="ctrl"></param>
+        /// <param name="text"></param>
+#if MONO
+        public static void SetValue<T>(this Form form, T ctrl, string property, object obj) where T : Control
+#else
+        public static void SetValue<T>(this Form form, in T ctrl, string property, object obj) where T : Control
+#endif
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (ctrl.InvokeRequired)
+            {
+                SetTextCallback<T> d = new SetTextCallback<T>(SetValue);
+                form.Invoke(d, new object[] { form, ctrl, property, obj });
+            }
+            else
+            {
+                ctrl.GetType().GetProperty(property).SetValue(ctrl, obj);
+            }
+        }
+    }
+}
